4438bafab655e1d99a37fcc13604dd0935748db8
Mattia Barbon <mattia.barbon@libero.it>
2011-08-03 06:28:16 +0200

    Added testing project skeleton.

---
+++ tests/src/org/barbon/acash/NewExpenseTest.java
@@ -0,0 +1,11 @@
+package org.barbon.acash;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class NewExpenseTest
+    extends ActivityInstrumentationTestCase2<NewExpense> {
+
+    public NewExpenseTest() {
+        super("org.barbon.acash", NewExpense.class);
+    }
+}
